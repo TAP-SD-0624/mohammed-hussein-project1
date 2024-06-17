1dce433b51b7207535daf8ce8026d4a505b72dce
Mohammed <moh9_kamal@hotmail.com>
2024-06-18 00:00:42 +0300

    update

---
+++ app.js
@@ -0,0 +1,52 @@
+// const search = document.querySelector("#search");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.set("Views", path.join(__dirname, "Views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "data", "topics.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedTopics = JSON.parse(fileData);
+  let topicId = req.params.id;
+  res.render("index", {
+    topics: storedTopics,
+    numberOfTopics: storedTopics.length,
+    id: topicId,
+  });
+});
+
+app.get(`/details/:id`, (req, res) => {
+  const filePath = path.join(__dirname, "data", "topics.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedTopics = JSON.parse(fileData);
+  let id = req.params.id;
+  res.render("details", { topics: storedTopics, topicId: id });
+});
+
+app.post("", (req, res) => {
+  let search = req.body.search;
+  const filePath = path.join(__dirname, "data", "topics.json");
+  const fileData = fs.readFileSync(filePath);
+  let storedTopics = JSON.parse(fileData);
+
+  const filteredTopics = storedTopics.filter(
+    (topic) =>
+      topic.topic.includes(`${search.toUpperCase()}`) ||
+      topic.topic.includes(`${search.toLowerCase()}`)
+  );
+
+  res.render("searched", {
+    topics: storedTopics,
+    numberOfTopics: filteredTopics.length,
+    search: search,
+  });
+});
+
+app.listen(3000);
---
+++ public/details.js
@@ -0,0 +1,42 @@
+const darkBtn = document.querySelector(".dark");
+const favBtn = document.querySelector(".fav");
+const favourites = document.querySelector(".favourites");
+
+function setTheme(theme) {
+  return document.getElementById("theme").setAttribute("href", theme);
+}
+
+darkBtn.addEventListener("click", function () {
+  const theme = document.getElementById("theme").getAttribute("href");
+  switch (theme) {
+    case "/light.css": {
+      return setTheme("/dark.css");
+    }
+    case "/dark.css": {
+      return setTheme("/light.css");
+    }
+  }
+});
+
+favBtn.addEventListener("click", function () {
+  favourites.classList.toggle("hidden");
+});
+
+window.onscroll = function () {
+  makeSticky();
+};
+
+// Get the header
+var header = document.querySelector(".header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeSticky() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
